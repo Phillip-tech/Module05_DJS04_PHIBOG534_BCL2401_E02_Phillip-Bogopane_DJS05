aa9ee2c1c3a52ecb01a419deaa3feecf4a86c96d
Phillip Bogopane <pm.bogopane@gamil.com>
2024-05-24 13:26:19 +0200

    Coded a code that defines two functions: getState and dispatch using switch case Conditional  Statement,.Commented about the code.

--- scripts.js
+++ scripts.js
@@ -11,3 +11,26 @@ let state = {
     SUBTRACT: "SUBTRACT",
     RESET: "RESET"
   };
+
+   // State management functions
+   const getState = () => {
+    return state;
+  };
+  
+  const dispatch = (action) => {
+    switch (action.type) {
+      case ACTIONS.ADD:
+        state.count += 1;
+        break;
+      case ACTIONS.SUBTRACT:
+        state.count -= 1;
+        break;
+      case ACTIONS.RESET:
+        state.count = 0;
+        break;
+      default:
+        console.log("Invalid action");
+    }
+    
+    console.log("State after dispatch:", state);
+  };
